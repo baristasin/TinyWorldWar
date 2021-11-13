351f22917a01c7b0bc2fe9a497ae2c9b957ec6ec
tasinbaris <tasinbaris95@gmail.com>
2021-11-13 16:42:14 +0300

    Jumping and gravity mechanics added

--- Assets/Game/Scripts/Behaviours/PlayerMovementBehaviour.cs
+++ Assets/Game/Scripts/Behaviours/PlayerMovementBehaviour.cs
@@ -1,3 +1,4 @@
+using Sirenix.OdinInspector;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,13 +11,37 @@ public class PlayerMovementBehaviour : MonoBehaviour
 
     [SerializeField] private Transform _cameraTransfom;
 
+    [SerializeField] private Transform _groundCheck;
+
+    [SerializeField] private LayerMask _groundMask;
+
+    [SerializeField] private float _jumpHeight;
+
+    private float _groundDistance = 0.4f;
     private float _turnSmoothTime = 0.1f;
     private float _turnSmoothVelocity;
+    private bool _isGrounded;
+    private bool _isFiring;
+    private Vector3 _fallVelocity;
+
+
+    [Button]
+    public void SetFiring()
+    {
+        _isFiring = !_isFiring;
+    }
 
     private void Update()
     {
+        _isGrounded = Physics.CheckSphere(_groundCheck.position, _groundDistance, _groundMask);
+
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
+
+        if(_isGrounded && _fallVelocity.y < 0)
+        {
+            _fallVelocity.y = Physics.gravity.y / 4.2f;
+        }
 
         Vector3 direction = new Vector3(horizontal,0, vertical).normalized;
 
@@ -31,5 +56,24 @@ public class PlayerMovementBehaviour : MonoBehaviour
             Vector3 moveDirection = Quaternion.Euler(0, targetAngle, 0) * Vector3.forward;
             _characterController.Move(moveDirection.normalized * _speed * Time.deltaTime);
         }
+
+        if (_isFiring)
+        {
+            float targetAngle =  _cameraTransfom.eulerAngles.y;
+
+            float angle = targetAngle;
+
+            transform.rotation = Quaternion.Euler(0, angle, 0);
+        }
+
+        if (Input.GetButtonDown("Jump") && _isGrounded)
+        {
+            _fallVelocity.y = Mathf.Sqrt(_jumpHeight * Physics.gravity.y / 4.2f * Physics.gravity.y);
+        }
+
+
+        _fallVelocity.y += Physics.gravity.y * Time.deltaTime;
+
+        _characterController.Move(_fallVelocity * Time.deltaTime);
     }
 }
