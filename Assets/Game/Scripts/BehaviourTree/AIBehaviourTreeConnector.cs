a8df9788892b0fc167c5d85cb0d6da0f26b44d09
tasinbaris <tasinbaris95@gmail.com>
2021-12-20 01:07:37 +0300

    AIBeh -> BehaviourTree connector added

---
+++ Assets/Game/Scripts/BehaviourTree/AIBehaviourTreeConnector.cs
@@ -0,0 +1,19 @@
+﻿using Assets.Game.Scripts.Controllers;
+using System.Collections;
+using UnityEngine;
+
+namespace Assets.Game.Scripts.BehaviourTree
+{
+    public class AIBehaviourTreeConnector : MonoBehaviour
+    {
+        public SoldierCharacterController SoldierCharacterController => _soldierCharacterController;
+
+        private SoldierCharacterController _soldierCharacterController;
+
+        public void Initialize(SoldierCharacterController soldierCharacterController)
+        {
+            _soldierCharacterController = soldierCharacterController;
+        }
+
+    }
+}
