4fb69edcc9ecc4c659837325cc612e0395de9464
tasinbaris <tasinbaris95@gmail.com>
2021-12-20 01:08:00 +0300

    Basic HealthNode + RangeNode added

---
+++ Assets/Game/Scripts/BehaviourTree/Nodes/HealthNode.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Game.Scripts.BehaviourTree.Nodes
+{
+    public class HealthNode : Node
+    {
+        private AIBehaviourTreeConnector _connector;
+        private float _lowHealthThreshold;
+
+        public override NodeState Evaluate()
+        {
+            return _connector.SoldierCharacterController.AICharacterController.CharacterHealthBehaviour.CurrentHealth < _lowHealthThreshold 
+                ? NodeState.SUCCESS 
+                : NodeState.FAILURE;
+        }
+    }
+}
---
+++ Assets/Game/Scripts/BehaviourTree/Nodes/RangeNode.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Game.Scripts.BehaviourTree.Nodes
+{
+    public class RangeNode : Node
+    {
+        private float _range;
+        private Transform _target;
+        private Transform _origin;
+
+        public RangeNode(float range, Transform target, Transform origin)
+        {
+            _range = range;
+            _target = target;
+            _origin = origin;
+        }
+
+        public override NodeState Evaluate()
+        {
+            float distance = Vector3.Distance(_target.position, _origin.position);
+            return distance <= _range ? NodeState.SUCCESS : NodeState.FAILURE;
+        }
+    }
+}
