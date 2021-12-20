50d85a57390426003bba68eeca93dbf1cd170fa7
tasinbaris <tasinbaris95@gmail.com>
2021-12-21 01:50:52 +0300

    IsThereAnEnemy and Shoot nodes added

---
+++ Assets/Game/Scripts/BehaviourTree/Nodes/IsThereAnEnemyNearNode.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Game.Scripts.BehaviourTree.Nodes
+{
+    public class IsThereAnEnemyNearNode : Node
+    {
+        private AIBehaviourTreeConnector _connector;
+
+        public override NodeState Evaluate()
+        {
+            return _connector.SoldierCharacterController.AICharacterController.AIEnemyRadarBehaviour.IsThereAnEnemyNear
+                ? NodeState.SUCCESS
+                : NodeState.FAILURE;
+        }
+    }
+}
---
+++ Assets/Game/Scripts/BehaviourTree/Nodes/ShootNode.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Game.Scripts.BehaviourTree.Nodes
+{
+    public class ShootNode : Node
+    {
+        private AIBehaviourTreeConnector _connector;
+
+        public override NodeState Evaluate()
+        {
+            _connector.SoldierCharacterController.AICharacterController.AIaimBehaviour.
+                SetAimTarget(_connector.SoldierCharacterController.AICharacterController.AIEnemyRadarBehaviour.CurrentEnemyTransform);
+
+            return NodeState.RUNNING;
+        }
+    }
+}
