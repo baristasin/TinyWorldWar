7d00f2a65513012512a3c93b0d25fedff0649d40
tasinbaris <tasinbaris95@gmail.com>
2021-12-21 01:51:05 +0300

    GoNearestHospitalNode added

---
+++ Assets/Game/Scripts/BehaviourTree/Nodes/GoNearestHospitalNode.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Game.Scripts.BehaviourTree.Nodes
+{
+    public class GoNearestHospitalNode : Node
+    {
+        private AIBehaviourTreeConnector _connector;
+
+        public override NodeState Evaluate()
+        {
+            _connector.SoldierCharacterController.AICharacterController.AIMovementBehaviour.
+                SetTargetPosition(_connector.SoldierCharacterController.GameManager.HospitalController.HospitalTransforms[0].position);
+
+            return NodeState.RUNNING;
+        }
+    }
+}
