783b5c658d747997f7151aaf62f8af830c80373f
tasinbaris <tasinbaris95@gmail.com>
2022-01-09 20:25:50 +0300

    Navmesh updates

---
+++ Assets/Game/Scripts/Behaviours/DummyPlaneBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using Cinemachine;
+using System.Collections;
+using UnityEngine;
+
+namespace Assets.Game.Scripts.Behaviours
+{
+    public class DummyPlaneBehaviour : MonoBehaviour
+    {
+        [SerializeField] private CinemachineSmoothPath _path;
+        [SerializeField] private float _speed;
+
+        private float _distance;
+
+        private void Update()
+        {
+            transform.position = _path.EvaluatePositionAtUnit(_distance, CinemachinePathBase.PositionUnits.Distance);
+            transform.rotation = _path.EvaluateOrientationAtUnit(_distance, CinemachinePathBase.PositionUnits.Distance) * Quaternion.Euler(-90f, 0, 180f);
+
+            _distance += _speed * Time.deltaTime;
+
+            if (_distance >= _path.PathLength)
+            {
+                _distance = 0;
+            }
+
+        }
+    }
+}
